445bfa8dbe64f2465e74a3db69fbbdbcc3715d83
Ruslan_Adelgareev <Mega.Adelgareev@mail.ru>
2023-02-17 23:16:15 +0300

    Макиато в деле

--- LapTop.py
+++ LapTop.py
@@ -6,9 +6,20 @@ def chocolate(name):
             sklad[1] = sklad[1] - 1 
             sklad[2] = sklad[2] - 1
             return "Оу-е кофе"
+        else:
+            return "Наши запасы не равны вашим запросам"
+    elif name == "Маккиато":
+        if sklad[0] >= 4 and sklad[1] >= 4 and sklad[2] >= 4:
+            sklad[0] = sklad[0] - 4
+            sklad[1] = sklad[1] - 4 
+            sklad[2] = sklad[2] - 4
+            return "Оу-е макиато, единственный и не повторимый"
+        else:
+            return "Наши запасы не равны вашим запросам"
     else:
         return "Иди-ка отсюда, кофе ему не нравится"
         
         
 sklad = [4, 4, 4]  # Молоко, сливки, зерна
 print(chocolate(input('Пить или есть?')))
+#Тут есть ошибка
